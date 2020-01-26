a8314551010a73afbb63b587657f99f6717534c1
MFowler <exactmark@gmail.com>
2020-01-26 17:02:28 -0500

    Extracted solver code.
    Basic Astar seems to be working, but need to make trackback.

---
+++ Solver.go
@@ -0,0 +1,73 @@
+package main
+
+import "container/heap"
+
+type Solver struct {
+	solutionMemo map[string]*SequentialInterface
+	memoQueue    []*SequentialInterface
+	memoSuccess  int
+	greedy       bool
+}
+
+func createSolver() *Solver {
+
+	memoQueue := make([]*SequentialInterface, 0)
+	solutionMemo := make(map[string]*SequentialInterface)
+
+	returnedSolver := Solver{
+		solutionMemo: solutionMemo,
+		memoQueue:    memoQueue,
+		memoSuccess:  0,
+	}
+
+	return &returnedSolver
+}
+
+func (solver *Solver) solve(startState *SequentialInterface, greedy bool) *SequentialInterface {
+	var repeatedStates int64
+	frontierQueue := make(PriorityQueue, 0)
+
+	exploredStateCache := make(map[string]*SequentialInterface, 0)
+
+	heap.Init(&frontierQueue)
+	var priority int
+	if greedy {
+		priority = (*startState).getH()
+	} else {
+		priority = (*startState).getExpectedCost()
+	}
+	frontierQueue.PushSequentialInterface(startState, priority)
+
+	childList := (*startState).getChildren()
+	for _, val := range childList {
+		frontierQueue.PushSequentialInterface(val, (*val).getH())
+	}
+
+	for frontierQueue.Len() > 0 {
+		exploringNode := *frontierQueue.PopSequentialInterface()
+		if exploringNode.isGoal() {
+			if !greedy {
+				//	store to solutionMemo cache
+			}
+			return &exploringNode
+		}
+		exploredStateCache[exploringNode.getStateIdentifier()] = &exploringNode
+		childList := exploringNode.getChildren()
+		for _, singleChild := range childList {
+			_, existingState := exploredStateCache[(*singleChild).getStateIdentifier()]
+			if existingState {
+				repeatedStates += 1
+			} else {
+				var childPriority int
+				if greedy {
+					childPriority = (*singleChild).getH()
+				} else {
+					childPriority = (*singleChild).getExpectedCost()
+				}
+				frontierQueue.PushSequentialInterface(singleChild,childPriority)
+			}
+		}
+	}
+	panic("unable to find solution")
+}
+
--- nPuzzleMain.go
+++ nPuzzleMain.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"container/heap"
 	"fmt"
 	//"math"
 	//"math/rand"
@@ -24,65 +23,6 @@ func describe(i interface{}) {
 	fmt.Printf("(%v, %T)\n", i, i)
 }
 
-
-type Solver struct {
-	solutionMemo map[string]*SequentialInterface
-	memoQueue    []*SequentialInterface
-	memoSuccess  int
-	greedy       bool
-}
-
-func createSolver() *Solver {
-
-	memoQueue := make([]*SequentialInterface, 0)
-	solutionMemo := make(map[string]*SequentialInterface)
-
-	returnedSolver := Solver{
-		solutionMemo: solutionMemo,
-		memoQueue:    memoQueue,
-		memoSuccess:  0,
-		greedy:       true,
-	}
-
-	return &returnedSolver
-}
-
-func insertAt(myArray *[]*SequentialInterface, item *SequentialInterface, index int) *[]*SequentialInterface {
-
-	actingArray := *myArray
-	actingArray = append(actingArray, nil)
-	copy(actingArray[index+1:], actingArray[index:])
-	actingArray[index] = item
-	return &actingArray
-}
-
-func (solver *Solver) solve(startState *SequentialInterface, greedy bool) *SequentialInterface {
-
-	frontierQueue := make(PriorityQueue, 0)
-
-	heap.Init(&frontierQueue)
-	var priority int
-	if greedy{
-		priority=(*startState).getH()
-	}	else{
-		priority=(*startState).getExpectedCost()
-	}
-	frontierQueue.PushSequentialInterface(startState, priority)
-
-	childList:= (*startState).getChildren()
-	for _,val := range(childList){
-		frontierQueue.PushSequentialInterface(val,(*val).getH())
-	}
-
-	for frontierQueue.Len() > 0 {
-		item := frontierQueue.PopSequentialInterface()
-		fmt.Printf("%v\n",(*item).getH())
-		describe(*item)
-		//fmt.Printf("%.2d:%s ", item.priority, item.value)
-	}
-	panic("implement me")
-}
-
 func main() {
 
 	nSize := 3
@@ -94,7 +34,7 @@ func main() {
 
 	mySolver := createSolver()
 
-	mySolver.solve(&startState, true)
+	mySolver.solve(&startState, false)
 
 	//fmt.Printf("(%v, %T)\n", goalState, goalState)
 
